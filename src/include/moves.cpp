5e6909817b532b4e4f756849acdaf7cb76755347
Sulove mosharaf <111306308+Sulovemosharaf004@users.noreply.github.com>
2023-07-21 07:33:49 +0600

    Writing with CGPT

--- src/include/moves.cpp
+++ src/include/moves.cpp
@@ -1,6 +1,22 @@
 #include "moves.hpp"
 #include <iostream>
 
+std::pair<int,int>* Pawn::getMoves(const Cboard& cboard ,int& numMoves  ) const {
+    std::pair<int,int>* moves = new std::pair<int,int >[2];
+    numMove = 0;
+     
+     int newX = posX+1;
+     int newY =  posY+1;
+
+     if(cboard.isValidSquare(newX, newY)&& cboard(newX,newY )==0){
+        moves[numMoves++]={newX,newY};
+
+
+     }
+     return moves ; 
+
+}
+
 
 
 
--- src/include/moves.hpp
+++ src/include/moves.hpp
@@ -1,5 +1,80 @@
 #ifndef MOVES_hpp
 #define MOVES_hpp
+#include <iostream>
+
+class Cboard;
+// BAse class for any chess piece
+class piece {
+    public :
+    piece() : posX(0),posY(0){}
+    virtual ~piece () { }
+    virtual std::pair<int, int>*getMoves (const Cboard& cboard, int& numMoves) const  0;
+    void setPosition(int x , int y){
+        posX = x;
+        posY = y;
+    }
+    private :
+    int posX;
+    int posY;
+};
+class Pawn : public piece {
+    using piece::piece;
+    virtual std::pair<int,int>*getMoves(const Cboard& cboard , int& numMoves) const override ;
+};
+
+class Bishop : public piece{
+    using piece::piece;
+    virtual std::pair<int,int>* getMoves(const Cboard& cboard , int& numMoves) const override ;
+    };
+
+class Rook : public piece{
+    using piece::piece;
+    virtual std::pair<int,int>* getMoves(const Cboard& cboard , int& numMoves) const override ;
+    };
+class Knight : public piece{
+    using piece::piece;
+    virtual std::pair<int,int>* getMoves(const Cboard& cboard , int& numMoves) const override ;
+    };
+
+class Queen : public piece{
+    using piece::piece;
+    virtual std::pair<int,int>* getMoves(const Cboard& cboard , int& numMoves) const override ;
+    };
+class King : public piece{
+    using piece::piece;
+    virtual std::pair<int,int>* getMoves(const Cboard& cboard , int& numMoves) const override ;
+    };
+class Cboard{
+    public :
+    Cboard();
+    void placepiece(piece*piece , int x , int y);
+    int operator()(int x , int y) const ;
+    void printCbord() const ;
+
+    private :
+    int cboard[8][8];
+
+
+};
+
+#endif 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
